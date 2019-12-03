5fe1c66d5de16b2ac52d57154521c95b5b369009
myname21-meet <myname21@meet.mit.edu>
2019-12-03 13:01:55 -0500

    perfec day

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 @app.route("/")
 def home():
 	return render_template("home.html")
- @app.route("/store")
- def store():
- 	return render_template("store.html")
 
+@app.route("/store")
+def store():
+	return render_template("store.html")
+
+@app.route("/cart")
+def cart():
+	return render_template("cart.html")
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
 #####################
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
